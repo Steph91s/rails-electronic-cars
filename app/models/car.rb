b39d95fa2d2941aa00015bcab907b1ed895d0ab7
Nino Lindenberg <ninovationlab@gmail.com>
2020-08-17 23:38:48 +0200

    cars controller added === REMEMBER TO ADD :PHOTO after DB for IMGAES

--- app/models/car.rb
+++ app/models/car.rb
@@ -7,5 +7,5 @@ class Car < ApplicationRecord
   validates :model, :brand, :location, presence: true
   validates :price_per_day, presence: true, numericality: { greater_than: 0 }  
   validates :number_of_seat, presence: true, inclusion: { in: [1, 2, 3, 4] }
-  geocoded_by :location
+  #geocoded_by :location
 end
