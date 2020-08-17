ecfcf24d23b6b8821007350f7c42a1386c0a6c93
Steffen Staengle <steffen.staengle@gmail.com>
2020-08-17 17:46:09 +0200

    Fix typo

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,7 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :category, presence: true, inclusion; { in: CATEGORIES }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :model, :brand, :location, presence: true
   validates :price_per_day, presence: true, numericality: { greater_than: 0 }  
   validates :number_of_seat, presence: true, inclusion: { in: [1, 2, 3, 4] }
