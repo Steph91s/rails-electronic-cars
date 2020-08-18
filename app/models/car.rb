1eb9893ee8591dadb5e8cc4209990f6b7f53c676
Fiona Toublanc <fionatoublanc@gmail.com>
2020-08-18 17:18:30 +0200

    cloudinary done

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,11 +2,11 @@ class Car < ApplicationRecord
   CATEGORIES = ["BWM", "VW", "Tesla", "Mercedes Benz", "Toyota", "Hyundai", "BYD"]
   belongs_to :user
   has_many :bookings
-  #has_one_attached :photo => Cloudinary implementation
+  has_one_attached :photo
 
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :model, :brand, :location, presence: true
-  validates :price_per_day, presence: true, numericality: { greater_than: 0 }  
+  validates :price_per_day, presence: true, numericality: { greater_than: 0 }
   validates :number_of_seat, presence: true, inclusion: { in: [1, 2, 3, 4] }
   #geocoded_by :location
 end
