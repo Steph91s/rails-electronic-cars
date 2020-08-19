c81ef1f6a3a8a13b3366e5e8b07cbf94dd381db1
Nino Lindenberg <ninovationlab@gmail.com>
2020-08-19 14:45:29 +0200

    Geocode applied REMEMBER to use CLOUDINARY KEY and API GMAP

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,7 +6,7 @@ class Car < ApplicationRecord
   has_one_attached :photo
 
   validates :brand, presence: true, inclusion: { in: BRAND }
-  validates :model, :brand, :location, presence: true
+  validates :model, :description, :location, presence: true
   validates :price_per_day, presence: true, numericality: { greater_than: 0 }
   validates :number_of_seat, presence: true, inclusion: { in: [1, 2, 3, 4] }
   geocoded_by :location
