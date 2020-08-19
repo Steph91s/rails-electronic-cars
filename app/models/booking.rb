0630d8e6de704311d1090097a4b1dc2bd22cd7b5
Steffen Staengle <steffen.staengle@gmail.com>
2020-08-20 00:37:34 +0200

    User dashboard view, controller & model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,15 @@ class Booking < ApplicationRecord
   validates :user, :car, presence: true
   validates :check_in_date, :check_out_date, presence: true
 
+  def self.hostings(user)
+    owner_cars = Car.where(user: user)
+    hostings = []
+    Booking.all.each do |booking|
+      hostings << booking if booking.car.user == user
+    end
+    return hostings
+  end
+
   private
   def total_price
     diff_days = Date.parse(self.check_out_date).mjd - Date.parse(self.check_in_date).mjd
