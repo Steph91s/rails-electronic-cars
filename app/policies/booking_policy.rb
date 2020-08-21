e3aba9fb0a44e07f5977ae7b52d7c8574326ebb4
Nino Lindenberg <ninovationlab@gmail.com>
2020-08-21 14:41:35 +0200

    DONE

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -8,4 +8,8 @@ class BookingPolicy < ApplicationPolicy
   def create?
     return true
   end
+
+  def validate?
+    record.car.user == user
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ fiona = User.create(email: 'fiona@ecars.com', password: '123456')
 
 [steffen, aron, nino, fiona].each { |member| users << member }
 
-locations = ["1600 Pennsylvania Avenue NW, Washington, DC 20500", "Westminster, London SW1A 1AA", "Panoramastraße 1A, 10178 Berlin", "Pariser Platz 3, 10117 Berlin", "Platz der Republik 1, 11011 Berlin", "Platz der Luftbrücke 5, 12101 Berlin", "Am Lustgarten, 10178 Berlin", "Bodestraße 1-3, 10178 Berlin", "Bernauer Straße 111, 13355 Berlin", "93 Kadoorie, Kowloon, Hongkong", "Friedrichstraße 43-45, 10969 Berlin", "Lassallestraße 40, 1020 Wien", "Van Ostadestraat 1, 1072 ER Amsterdam", "Rue d'Ulm 45, 75005 Paris", "Carrer de la Llacuna 128, 08018 Barcelona", "Turmstr. 18, 10559 Berlin", "Niederkirchnerstraße 8, 10963 Berlin", "Hardenbergplatz 8, 10787 Berlin", "Königin-Luise-Strasse 6-8, 14195 Berlin", "Karl-Liebknecht-Str. 1, 10178 Berlin"]
+locations = ["Eastern Pkwy 781, Brooklyn, NY 11213", "Westminster, London SW1A 1AA", "Panoramastraße 1A, 10178 Berlin", "Pariser Platz 3, 10117 Berlin", "Platz der Republik 1, 11011 Berlin", "93 Kadoorie, Kowloon, Hongkong", "Friedrichstraße 43-45, 10969 Berlin", "Lassallestraße 40, 1020 Wien", "Van Ostadestraat 1, 1072 ER Amsterdam", "Rue d'Ulm 45, 75005 Paris", "Carrer de la Llacuna 128, 08018 Barcelona", "Turmstr. 18, 10559 Berlin", "Niederkirchnerstraße 8, 10963 Berlin", "Karl-Liebknecht-Str. 1, 10178 Berlin"]
 
 cars = [
   {
@@ -58,8 +58,21 @@ cars = [
     photo: "https://evcompare.io/upload/resize_cache/iblock/b9a/768_512_2/b9a2b3978d675310c5d6eb16cd10bf2d.jpg", brand: "BYD", model: "e1", description: "Find parking even in the tightest of spaces", price_per_day: 15, number_of_seat: 4, category: "compact city"
   },
   {
-    photo: "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/byd-e6-rt-8_0.jpg?itok=3fr4xfKg", brand: "BYD", model: "e6", description: "The standard from China, available now in Europe!", price_per_day: 20, number_of_seat: 4, category: "family car"
+    photo: "https://images.unsplash.com/photo-1571990527526-2cfb6bf4f493?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80", brand: "Mercedes Benz", model: "EQC", description: "Another German beast for family or any adventure", price_per_day: 50, number_of_seat: 4, category: "all-rounder"
   },
+  {
+    photo: "https://images.unsplash.com/photo-1550085122-2a4948cda8c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1525&q=80", brand: "VW", model: "Golf", description: "Hybrid Golf electro AC motors", price_per_day: 55, number_of_seat: 4, category: "family car"
+  },
+  {
+    photo: "https://images.unsplash.com/photo-1594291464994-b62a3d06d7c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", brand: "BMW", model: "i3", description: "Standard and environtment frindly", price_per_day: 15, number_of_seat: 4, category: "family car"
+  },
+  {
+    photo: "https://images.unsplash.com/photo-1577796796409-d6ceea6f3359?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80", brand: "BMI", model: "i8", description: "Freude am Fahren, Leute!", price_per_day: 75, number_of_seat: 4, category: "all-rounder"
+  },
+  {
+    photo: "https://images.unsplash.com/photo-1491497875599-6c21f33c7e19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=961&q=80", brand: "Tesla", model: "Roadster", description: "The best e car sofar, have fun!", price_per_day: 40, number_of_seat: 4, category: "sports car"
+  },
+
 ]
 
 cars.each do |car|
